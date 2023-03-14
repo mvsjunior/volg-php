2914793386426c563bd923bdae5432f1c4ea15b3
Mauro <mauro.emc@outlook.com>
2023-03-13 21:51:13 -0300

    Implementado método para verificar se a rota acessada já foi registrada

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -6,7 +6,6 @@ class Router
 {
     private $routes;
 
-
     public function setRoutes($routes = [])
     {
         $this->routes = $routes;
@@ -15,5 +14,32 @@ class Router
     public function handle()
     {
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        if($this->verifyIfRouteIsRegistred($uri))
+        {
+            echo "Rota registrada";
+        }
+        else
+        {
+            echo "Rota não registrada";
+        }
+    }
+
+
+    public function verifyIfRouteIsRegistred($uri)
+    {
+        $rotaNaoRegistrada = false;
+        $rotaRegistrada    = true;
+
+        $route = isset($this->routes[$uri]) ? $this->routes[$uri] : "";
+
+        if(empty($route))
+        {
+            return $rotaNaoRegistrada;
+        }
+        else
+        {
+            return $rotaRegistrada;
+        }
     }
 }
